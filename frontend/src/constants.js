4d29284ac6dc12512158ff62d72a7a5ae007be00
anunadhu1234 <ag952006@student.nitw.ac.in>
2024-08-14 18:26:12 +0530

    build deployed

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
-//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = ''; // If using proxy
+//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : 'https://proshop-backend-h78p.onrender.com';
+export const BASE_URL = 'https://proshop-backend-h78p.onrender.com'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
