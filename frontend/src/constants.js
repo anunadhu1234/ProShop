f8585388f804089f55112c05bb41066702648198
anunadhu1234 <92930048+anunadhu1234@users.noreply.github.com>
2024-09-04 00:17:04 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 
 export const BASE_URL = 'https://proshop-bdd2.onrender.com'; // If using proxy
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const PRODUCTS_URL = 'https://proshop-bdd2.onrender.com/api/products';
+export const USERS_URL = 'https://proshop-bdd2.onrender.com/api/users';
+export const ORDERS_URL = 'https://proshop-bdd2.onrender.com/api/orders';
+export const PAYPAL_URL = 'https://proshop-bdd2.onrender.com/api/config/paypal';
